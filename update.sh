f82caca7fd4b4d2151e94428d2a212cd5a35662a
clem9669 <18504086+clem9669@users.noreply.github.com>
2024-01-30 17:01:55 +0000

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -uo pipefail
+IFS=$'\n\t'
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+
+# Check for updates
+git fetch
+
+# Compare the local and remote versions
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse @{u})
+
+# If updates are available, prompt the user
+if [ "$LOCAL" != "$REMOTE" ]; then
+    echo "Update available for Kali-build."
+    while true; do
+        read -p "Would you like to update? [Y/n] " response
+        case $response in
+            [Yy]* | "" )
+                git pull
+                bash -x install.sh
+                echo "Kali-build has been updated."
+                break
+                ;;
+            [Nn]* )
+                echo "Update canceled."
+                exit
+                ;;
+            * )
+                echo "Please answer Y or n."
+                ;;
+        esac
+    done
+else
+    echo "Already up-to-date 🌹"
+fi
