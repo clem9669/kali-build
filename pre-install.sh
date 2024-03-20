5153d5905ff246ad84bea35a7d5b9e334d46f8f3
clem9669 <18504086+clem9669@users.noreply.github.com>
2024-03-20 13:42:24 +0000

    Update pre-install.sh
    Fixing

--- pre-install.sh
+++ pre-install.sh
@@ -8,11 +8,11 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y pipx curl git wget whiptail neofetch ufw feh pipx
 
 echo "[*] Installing Pipx & Ansible"
-pipx install ansible --force
+pipx install ansible --force --include-deps
 pipx ensurepath
 export PATH="$PATH:$HOME/.local/bin"
 
 echo "[*] Installing Ansible Galaxy Collections"
-ansible-galaxy install -r requirements.yml
+$HOME/.local/bin/ansible-galaxy install -r requirements.yml
 echo "[*] All good, now run:"
 echo "bash install.sh"
